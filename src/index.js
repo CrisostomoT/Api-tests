e20a91592152cf2d8901ed0e4d606e1561cb2011
CrisT <crisostomotorres@hotmail.com>
2020-02-25 20:21:05 -0300

    apis tests

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {request} from './requestHelper';
+import { request } from './requestHelper';
 import './index.scss';
 
 const title = document.getElementById('title');
@@ -6,22 +6,18 @@ const title = document.getElementById('title');
 title.innerText = 'Pelis de Mario';
 
 // Aqui le decimos que busque peliculas que tengan el nombre Mario
-const peli = "Mario"
-const url = `https://movie-database-imdb-alternative.p.rapidapi.com/?page=1&r=json&s=${peli}`;
+const cantidad = prompt('cantidad a convertir');
+const desde = prompt('desde que moneda/criptomoneda');
+const hacia = prompt('a que moneda/criptomoneda');
+const url = `https://bravenewcoin-v1.p.rapidapi.com/convert?qty=${cantidad}&from=${desde}&to=${hacia}`;
 
 //La funcion request recibe 2 parametros, la url y el callback
-request(url, function(response){
-  // response es la variable que devuelve el archivo requestHelper en la linea 13
-  const content = document.getElementById('resultado');
-  content.innerHTML = '';
-  
-  // como response estipo string, tenemos que hacer JSON.parse para convertirlo en objeto y poder trabajar
-  JSON.parse(response).Search.forEach(peli => {
-    content.innerHTML += `
-      <div class="peli">
-        <h2>${peli.Title}</h2>
-        <img src="${peli.Poster}" alt="${peli.Title}" />
-      </div>
-    `
-  });
+request(url, function(response) {
+    // response es la variable que devuelve el archivo requestHelper en la linea 13
+    //const content = document.getElementById('resultado');
+    //content.innerHTML = '';
+
+    // como response estipo string, tenemos que hacer JSON.parse para convertirlo en objeto y poder trabajar
+    JSON.parse(response);
+    alert(response);
 });
