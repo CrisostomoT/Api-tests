407cfb9a515d7bb38267ada84c218487edb76a76
Crisostomo T <crisostomotorres@hotmail.com>
2020-02-26 16:25:06 -0300

    api cryptocurrency

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import { request } from './requestHelper';
 import './index.scss';
 
-const title = document.getElementById('title');
 
-title.innerText = 'Pelis de Mario';
-
-// Aqui le decimos que busque peliculas que tengan el nombre Mario
-const cantidad = prompt('cantidad a convertir');
-const desde = prompt('desde que moneda/criptomoneda');
-const hacia = prompt('a que moneda/criptomoneda');
+const cantidad = prompt('quantity') //document.querySelector('#quantity').value;
+const desde = prompt('from') //document.querySelector('#fromCurrency').value;
+const hacia = prompt('to') //document.querySelector('#toCurrency').value;
 const url = `https://bravenewcoin-v1.p.rapidapi.com/convert?qty=${cantidad}&from=${desde}&to=${hacia}`;
 
 //La funcion request recibe 2 parametros, la url y el callback
@@ -18,5 +14,6 @@ request(url, function(response) {
     //content.innerHTML = '';
 
     // como response estipo string, tenemos que hacer JSON.parse para convertirlo en objeto y poder trabajar
-    JSON.parse(response);
+    let datos = JSON.parse(response);
+    datos.innerHTML = '';
 });
