9405b3c1af79d4062c500afcec7fc3def7b036df
CrisT <crisostomotorres@hotmail.com>
2020-03-06 07:28:00 -0300

    Idk what I did, but I did something

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
-import { request } from './requestHelper';
 import './index.scss';
 
 
-const cantidad = prompt('quantity') //document.querySelector('#quantity').value;
-const desde = prompt('from') //document.querySelector('#fromCurrency').value;
-const hacia = prompt('to') //document.querySelector('#toCurrency').value;
-const url = `https://bravenewcoin-v1.p.rapidapi.com/convert?qty=${cantidad}&from=${desde}&to=${hacia}`;
-const resultado = document.querySelector('#result').addEventListener('click');
-
-//La funcion request recibe 2 parametros, la url y el callback
-request(url, function(response) {
-    // response es la variable que devuelve el archivo requestHelper en la linea 13
-    //const content = document.getElementById('resultado');
-    //content.innerHTML = '';
-
-    // como response estipo string, tenemos que hacer JSON.parse para convertirlo en objeto y poder trabajar
-    let datos = JSON.parse(response);
+document.querySelector('#convert').addEventListener("click", function btnConvert() {
+    const cantidad = document.querySelector('#quantity').value;
+    const desde = document.querySelector('#fromCurrency').value;
+    const hacia = document.querySelector('#toCurrency').value;
+    const url = `https://bravenewcoin-v1.p.rapidapi.com/convert?qty=${cantidad}&from=${desde}&to=${hacia}`;
+    const output = document.querySelector('#currencyOutput');
+    fetch(url, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "bravenewcoin-v1.p.rapidapi.com",
+                "x-rapidapi-key": "5318ddcf82msha503012fa24ea57p1e9fe9jsne71607a35fd0"
+            }
+        })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            output.innerHTML += `
+      <div class="card">
+      <div class="card-body">
+        <h2>${data.to_quantity}</h2>
+      </div>
+      </div>
+    `
+        });
 });
